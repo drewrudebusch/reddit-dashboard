ec8ab536b89f9aafb847da8c8b3ce85b065d1043
drewrudebusch <drewrudebusch@gmail.com>
2016-05-25 09:04:05 -0700

    All results displaying on the page with thumbnail features

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -11,17 +11,18 @@ app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
   }
   $scope.searchTerm = '';
   $scope.currentSearch = '';
-  $scope.thumbnailPosts = []
+  $scope.thumbnailPosts = [];
+  $scope.posts = [];
 
   $scope.clearSearch = function() {
     $scope.searchTerm = "";
   }
 
   $scope.searchReddit = function() {
-    
     if ($scope.searchHistory.indexOf($scope.searchTerm) < 0) {
       $scope.searchHistory.unshift($scope.searchTerm);
       $scope.currentSearch = $scope.searchTerm;
+      localStorage.setItem('currentSearch', $scope.currentSearch);
 
       var req = {
         url: "https://www.reddit.com/search.json?",
@@ -35,10 +36,14 @@ app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
       $http(req).then(function success(res) {
         var posts = res.data.data.children
         console.log(posts);
+        $scope.posts = [];
+        $scope.thumbnailPosts = [];
         for (var i = 0; i < posts.length; i++) {
           var tn = posts[i].data.thumbnail
-          if (tn !== 'self' && tn !== "") {
-            $scope.thumbnailPosts.push(posts[i])
+          if (tn !== 'self' && tn !== "" && $scope.thumbnailPosts.length < 4) {
+            $scope.thumbnailPosts.push(posts[i]);
+          } else {
+            $scope.posts.push(posts[i]);
           }
         }
         console.log('Thumbnail list: ', $scope.thumbnailPosts);
@@ -52,11 +57,32 @@ app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
     localStorage.setItem('searchHistory', JSON.stringify($scope.searchHistory))
   };
 
-$scope.historyLinkClicked = function(search) {
-  $scope.searchHistory.splice($scope.searchHistory.indexOf(search), 1);
-  $scope.searchTerm = search;
-  $scope.searchReddit();
-}
+  $scope.historyLinkClicked = function(search) {
+    $scope.searchHistory.splice($scope.searchHistory.indexOf(search), 1);
+    $scope.searchTerm = search;
+    $scope.searchReddit();
+  }
+
+  if (localStorage.currentSearch) {
+    console.log('currentSearch exists');
+      $scope.searchTerm = localStorage.currentSearch;
+      $scope.searchReddit();
+  }
+
+  $scope.clearAllHistory = function() {
+    localStorage.searchHistory = [];
+    $scope.searchHistory = [];
+    localStorage.currentSearch = "";
+    $scope.currentSearch = "";
+    $scope.thumbnailPosts = [];
+    $scope.posts = [];
+  }
+
+  $scope.clearHistoryItem = function(item) {
+    $scope.searchHistory.splice($scope.searchHistory.indexOf(item), 1);
+    localStorage.setItem('searchHistory', JSON.stringify($scope.searchHistory))
+    $scope.searchTerm = '';
+  }
 
 
 
